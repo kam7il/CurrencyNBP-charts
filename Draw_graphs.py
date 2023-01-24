72348a51aaa36622a64f3cf4a2e17915b30a5ddf
Kam7il <118853875+kam7il@users.noreply.github.com>
2023-01-24 22:45:13 +0100

    Update

--- Draw_graphs.py
+++ Draw_graphs.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 from CurrencyNBP_get_data import prize_in_pln
 
-
 data = prize_in_pln()
-x = data[0]
-y = data[1]
+x_data = data[0]
+y_data = data[1]
 
 fig, ax1 = plt.subplots()
-fig.tight_layout(pad=4)
-ax1.plot(x, y, color="green", linewidth=3)
-ax1.tick_params("y", colors="blue")
-fig.autofmt_xdate()
-ax1.set(title=f"Chart of {data[2][0]}/PLN", xlabel="Date", ylabel="PLN")
+fig.canvas.manager.set_window_title(f"Chart of {data[2][0]}/PLN")  # tytuł okna
+ax1.plot(x_data, y_data, color="green", linewidth=3)  # linia właściwości
+ax1.tick_params("y", colors="blue")  # parametry skali y
+fig.autofmt_xdate()  # auto rotacja daty
+ax1.set(title=f"Chart of {data[2][0]}/PLN", xlabel="Date", ylabel="PLN")  # etykiety
+ax1.yaxis.grid(True)  # pomocnicze linie y
 plt.show()
